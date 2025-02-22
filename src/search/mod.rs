f9172d69cdf0831b3478133e37842d978acee3ae
AspectOfTheNoob <Mcthouacbb@gmail.com>
2025-02-22 11:11:26 -0500

    Add Perft

--- src/search/mod.rs
+++ src/search/mod.rs
@@ -1,2 +1,3 @@
 pub mod ab_solver;
 pub mod search;
+pub mod perft;
---
+++ src/search/perft.rs
@@ -0,0 +1,33 @@
+use crate::games::board::{Board, GameResult};
+
+pub fn perft<const SPLIT: bool, B: Board>(board: &mut B, depth: u32) -> u64 {
+	if depth == 0 || board.game_result() != GameResult::NONE {
+		return 1;
+	}
+
+	let mut nodes = 0;
+	for mv in board.gen_moves() {
+		board.make_move(mv);
+		let sub_nodes = perft_impl(board, depth - 1);
+		if SPLIT {
+			println!("{:?}: {}", mv, sub_nodes);
+		}
+		nodes += sub_nodes;
+		board.unmake_move();
+	}
+	nodes
+}
+
+fn perft_impl<B: Board>(board: &mut B, depth: u32) -> u64 {
+	if depth == 0 || board.game_result() != GameResult::NONE {
+		return 1;
+	}
+
+	let mut nodes = 0;
+	for mv in board.gen_moves() {
+		board.make_move(mv);
+		nodes += perft_impl(board, depth - 1);
+		board.unmake_move();
+	}
+	nodes
+}
