86fa17c907dea1d13c85308367dea461932e5c6e
AspectOfTheNoob <Mcthouacbb@gmail.com>
2025-02-22 20:06:42 -0500

    Hopefully working connect 4 impl

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod games;
 mod search;
 mod util;
 
-use games::{board::Board, hexapawn::HexapawnBoard, tictactoe::TicTacToeBoard};
+use games::{board::Board, connect4::Connect4Board, hexapawn::HexapawnBoard, tictactoe::TicTacToeBoard};
 use search::{
     ab_solver::ABSolver,
     perft::perft,
@@ -10,10 +10,14 @@ use search::{
 };
 
 fn main() {
-    let nodes = perft::<true, TicTacToeBoard>(&mut TicTacToeBoard::startpos(), 100);
+    /*let nodes = perft::<true, TicTacToeBoard>(&mut TicTacToeBoard::startpos(), 100);
     println!("Nodes: {}", nodes);
 
     let nodes = perft::<true, HexapawnBoard>(&mut HexapawnBoard::startpos(), 100);
+    println!("Nodes: {}", nodes);*/
+
+    let mut board = Connect4Board::from_fen("7/7/3y3/1yrr3/1rryyy1/1ryryr1 r").unwrap();
+    let nodes = perft::<true, Connect4Board>(&mut board, 11);
     println!("Nodes: {}", nodes);
 
     let board = TicTacToeBoard::from_fen("3/O2/X2 X").unwrap();
