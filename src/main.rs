524b86522648d25cda5d1e4bffa50704101106fa
AspectOfTheNoob <Mcthouacbb@gmail.com>
2025-02-22 20:14:10 -0500

    Tested some perft positions manually

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,9 @@ fn main() {
     let nodes = perft::<true, HexapawnBoard>(&mut HexapawnBoard::startpos(), 100);
     println!("Nodes: {}", nodes);*/
 
-    let mut board = Connect4Board::from_fen("7/7/3y3/1yrr3/1rryyy1/1ryryr1 r").unwrap();
-    let nodes = perft::<true, Connect4Board>(&mut board, 11);
+    // let mut board = Connect4Board::startpos();
+    let mut board = Connect4Board::from_fen("7/7/4r2/r3y1y/yry1y1r/ryr1y1r r").unwrap();
+    let nodes = perft::<true, Connect4Board>(&mut board, 9);
     println!("Nodes: {}", nodes);
 
     let board = TicTacToeBoard::from_fen("3/O2/X2 X").unwrap();
