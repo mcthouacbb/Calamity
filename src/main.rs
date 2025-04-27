cabec38a5960b85299c876bb48a5f17ecb98434b
AspectOfTheNoob <Mcthouacbb@gmail.com>
2025-04-27 12:31:02 -0400

    Basic UCI

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod perft;
 mod search;
 mod util;
 
+use std::{io::stdin, str::SplitWhitespace};
+
 use games::{
     board::Board,
     connect4::Connect4Board,
@@ -17,7 +19,106 @@ use search::{
 };
 use util::Square;
 
+pub fn parse_move(board: &ThreeCheckBoard, str: &str) -> three_check::Move {
+    let legal_moves = board.gen_moves();
+    for mv in legal_moves {
+        if mv.to_string() == str {
+            return mv;
+        }
+    }
+    three_check::Move::NULL
+}
+
+fn parse_startpos(curr_board: &mut ThreeCheckBoard, toks: &mut SplitWhitespace<'_>) {
+    *curr_board = ThreeCheckBoard::startpos();
+    if toks.next() == Some("moves") {
+        loop {
+            let mv_str = toks.next();
+            if let Some(str) = mv_str {
+                curr_board.make_move(parse_move(&curr_board, str));
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+fn parse_fen(curr_board: &mut ThreeCheckBoard, toks: &mut SplitWhitespace<'_>) {
+    let mut fen = toks.next().unwrap().to_string();
+    let mut tok = toks.next();
+    loop {
+        if tok == Some("moves") || tok == None {
+            break;
+        }
+        fen += " ";
+        fen += tok.unwrap();
+        tok = toks.next();
+    }
+    *curr_board = ThreeCheckBoard::from_fen(fen.as_str()).unwrap();
+    if tok == Some("moves") {
+        loop {
+            let mv_str = toks.next();
+            if let Some(str) = mv_str {
+                curr_board.make_move(parse_move(&curr_board, str));
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+fn select_random_move(board: &ThreeCheckBoard) -> three_check::Move {
+    let moves = board.gen_moves();
+    moves[rand::random_range(0..moves.len())]
+}
+
+fn run_three_check() {
+    let mut curr_board = ThreeCheckBoard::startpos();
+    loop {
+        let mut command = String::new();
+        stdin().read_line(&mut command).expect("Bad input");
+        let mut toks = command.split_whitespace();
+        match toks.next() {
+            Some("uci") => {
+                println!("id name calamity");
+                println!("id author mcthouacbb");
+                println!("uciok");
+            }
+            Some("isready") => {
+                println!("readyok");
+            }
+            Some("ucinewgame") => {
+                // empty for now
+            }
+            Some("position") => match toks.next() {
+                Some("startpos") => {
+                    parse_startpos(&mut curr_board, &mut toks);
+                }
+                Some("fen") => {
+                    parse_fen(&mut curr_board, &mut toks);
+                }
+                _ => {
+                    println!("info string invalid command");
+                }
+            },
+            Some("go") => {
+                println!("bestmove {}", select_random_move(&curr_board));
+            }
+            Some("d") => {
+                println!("{}", &mut curr_board);
+            }
+            Some("quit") => {
+                return;
+            }
+            _ => {
+                println!("info string invalid command");
+            }
+        }
+    }
+}
+
 fn main() {
+    run_three_check();
     // perft::run_perft_suite_file::<Connect4Board>("res/c4_perft.txt");
     /*let board = TicTacToeBoard::from_fen("3/O2/X2 X").unwrap();
     println!("{}", board);
@@ -32,8 +133,4 @@ fn main() {
     let mut solver = ABSolver::<HexapawnBoard>::new();
     let result = solver.search(&board, SearchLimits::default());
     println!("{:?}", result);*/
-
-    let mut board = three_check::ThreeCheckBoard::startpos();
-
-    println!("{}", board);
 }
